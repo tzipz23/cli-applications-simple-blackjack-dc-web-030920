527dedc7820cc3ae50641acf0f4028e70b94f957
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-05 00:49:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,6 +29,9 @@ def end_game(card_total)
 end
 
 def initial_round
+   card_total = deal_card + deal_card
+  display_card_total(card_total)
+  return card_total
   # code #initial_round here
 end
 
