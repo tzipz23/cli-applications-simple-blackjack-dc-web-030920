9b145e81cbeadf5260f701542eba19ee83fdeb6d
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-05 00:54:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,15 @@ def initial_round
 end
 
 def hit?(card_total)
-  
+  prompt_user
+  input = get_user_input
+  if input == 'h' then
+    card_total += deal_card
+    elsif input == 's' then
+      card_total
+      else
+       invalid_command
+    end
   # code hit? here
 end
 
