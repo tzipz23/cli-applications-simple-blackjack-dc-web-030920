379e9e374d22f4595457c566f1ea339ad0664ce0
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-05 00:34:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,7 +8,7 @@ def deal_card
   # code #deal_card here
 end
 
-def display_card_total
+def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
   # code #display_card_total here
 end
